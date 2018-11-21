e29a411ddebc2b83742e899bd1439e56e9e93636
root <root@localhost.localdomain>
2018-11-20 21:29:33 -0500

    Need write up

--- week/10/stub.py
+++ week/10/stub.py
@@ -2,7 +2,8 @@
 # from the git repo
 import md5py
 import struct
-
+import socket
+import time
 
 #####################################
 ### STEP 1: Calculate forged hash ###
@@ -12,15 +13,19 @@ host = '142.93.118.186'
 port = 1234
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-p = s.recv(1024)
-print(p)
 
-message = 'pleasehelpme\n'    # original message here
-s.send(message)
+p = s.recv(1024)
+s.send('1\n')
+
+s.recv(1024)
+
+message = 'pleasehelpme'    # original message here
+s.send(message+'\n')
 
 rec = s.recv(1024).strip()
 # Grab last 32
 legit = rec[-32:]     # a legit hash of secret + message goes here, obtained from signing a message
+s.recv(1024)
 
 # initialize hash object with state of a vulnerable hash
 fake_md5 = md5py.new('A' * 64)
@@ -33,7 +38,6 @@ fake_md5.update(malicious)
 
 # fake_hash is the hash for md5(secret + message + padding + malicious)
 fake_hash = fake_md5.hexdigest()
-print(fake_hash)
 
 
 #############################
@@ -48,24 +52,30 @@ print(fake_hash)
 # (i.e. 20 char msg = 160 bits = 0xa0 = '\xa0\x00\x00\x00\x00\x00\x00\x00\x00')
 # craft padding to align the block as MD5 would do it
 # (i.e. len(secret + message + padding) = 64 bytes = 512 bits
-for secret_len in range(6,16):
-	padding_count = 64 - len(message) - secret_len - 9
-	padding = '\x80' + ('\x00'*padding_count) + struct('(<Q',((secret_len + len(message))*8))
+for secret in range(6,16):
+	padding_count = 64 - len(message) - secret - 9
+	padding = '\x80' + ('\x00'*padding_count) + struct.pack('<Q',((secret + len(message))*8))
 
-# payload is the message that corresponds to the hash in `fake_hash`
-# server will calculate md5(secret + payload)
-#                     = md5(secret + message + padding + malicious)
-#                     = fake_hash
-payload = message + padding + malicious 
+	# payload is the message that corresponds to the hash in `fake_hash`
+	# server will calculate md5(secret + payload)
+	#                     = md5(secret + message + padding + malicious)
+	#                     = fake_hash
+	payload = message + padding + malicious 
 
-# send `fake_hash` and `payload` to server (manually or with sockets)
-# REMEMBER: every time you sign new data, you will regenerate a new secret!
+	# send `fake_hash` and `payload` to server (manually or with sockets)
+	# REMEMBER: every time you sign new data, you will regenerate a new secret!
+	
+	s.send('2\n')
+	s.recv(1024)
+	s.send(fake_hash + '\n')
+	s.recv(1024)
+	s.send(payload + '\n')
+	time.sleep(1)
+	data = s.recv(1024)
+	
+	if  "broken!" in data:
+		print data
+		break
 
-s.send('2\n')
-s.recv(1024)
-s.send(fake_hash +'\n')
-s.recv(1024)
-s.send(payload+'\n')
-time.sleep(1)
-data = s.recv(1024)
-print(data)
+	
+	
